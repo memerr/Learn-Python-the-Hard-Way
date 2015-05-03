87bbcb0c65ba87e422e7a6e8057f90f5d39ffbd7
Ricardo Raphael Joson <RrJoson@RrJosons-MacBook-Pro.local>
2015-05-03 23:48:05 +0800

    comments

--- ex4.py
+++ ex4.py
@@ -1,10 +1,18 @@
+# creates a variable called cars and sets a value of 100 to it
 cars = 100
+# creates a variable called space_in_a_car and sets a value of 4.0 to it
 space_in_a_car = 4.0
+# creates a variable called drivers and sets a value of 30 to it
 drivers = 30
+# creates a variable called passengers and sets a value of 90 to it
 passengers = 90
+# creates a variable called cars_not_driven and sets a value of (the values of the variable cars minus the value of the vraible drivers)
 cars_not_driven = cars - drivers
+# creates a variable called cars_driven and sets its value to the value of the variable drivers
 cars_driven = drivers
+# creates a variable called carpool_capacity and sets its value to the value of cars_driver multiplied by the value of space_in_a_car
 carpool_capacity = cars_driven * space_in_a_car
+# creates a variable called average_passengers_per_car and sets a value of (the value of passengers divided by the value of cars_driven)
 average_passengers_per_car = passengers / cars_driven
 
 
