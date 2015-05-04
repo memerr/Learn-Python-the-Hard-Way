c76311c7c78c8cc5dda268d71767a1b175e2b9e6
Ricardo Raphael Joson <RrJoson@RrJosons-MacBook-Pro.local>
2015-05-04 13:12:38 +0800

    comments

--- ex7.py
+++ ex7.py
@@ -1,22 +1,39 @@
+# prints a string
 print "Mary had a little lamb."
+# prints a string with a string format with a string as the parameter
 print "Its fleece was white as %s" % 'snow'
+# prints a string
 print "And everywhere that Mary went."
 # prints  a full stop 10 times
 print "." * 10 # what'd that do?
 
+# sets a C string to a variable called end1
 end1 = "C"
+# sets a string h to a variable called end2
 end2 = "h"
+# sets a string e to a variable called end3
 end3 = "e"
+# sets a string e to a variable called end4
 end4 = "e"
+# sets a string s to a variable called end5
 end5 = "s"
+# sets a string e to a variable called end6
 end6 = "e"
+# sets a string B to a variable called end7
 end7 = "B"
+# sets a string u to a variable called end8
 end8 = "u"
+# sets a string r to a variable called end9
 end9 = "r"
+# sets a string g to a variable called end10
 end10 = "g"
+# sets a string e to a variable called end11
 end11 = "e"
+# sets a string r to a variable called end12
 end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens (if removed, prints on a new line and not on the same line)
+# concatenates a set of strings
 print end1 + end2 + end3 + end4 + end5 + end6
+# prints on a new line
 print end7 + end8 + end9 + end10 + end11 + end12
