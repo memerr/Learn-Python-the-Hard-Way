2efa780f6d0fe67f33492882dfaf82ad90441882
Ricardo Raphael Joson <RrJoson@RrJosons-MacBook-Pro.local>
2015-05-04 13:06:15 +0800

    error

--- ex7.py
+++ ex7.py
@@ -1,6 +1,7 @@
 print "Mary had a little lamb."
-print "Its fleece was white as %s" 'snow'
+print "Its fleece was white as %s" % 'snow'
 print "And everywhere that Mary went."
+# prints  a full stop 10 times
 print "." * 10 # what'd that do?
 
 end1 = "C"
@@ -16,6 +17,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that comma at the end. try removing it to see what happens
+# watch that comma at the end. try removing it to see what happens (if removed, prints on a new line and not on the same line)
 print end1 + end2 + end3 + end4 + end5 + end6
 print end7 + end8 + end9 + end10 + end11 + end12
