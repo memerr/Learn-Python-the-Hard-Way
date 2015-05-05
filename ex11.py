4f7b6706dc31ead926c87b45097403a181f4a6b8
Ricardo Raphael Joson <RrJoson@RrJosons-MacBook-Pro.local>
2015-05-05 19:30:52 +0800

    initial commit

--- ex11.py
+++ ex11.py
@@ -7,3 +7,14 @@ weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % (
 	age, height, weight)
+
+# study drills
+
+print "What's your name?",
+name = raw_input()
+print "Hello " + name
+
+# math
+print "Give the measurement of a side of a square in centimeters:",
+x = int(raw_input()) ** 2
+print "The square has an area of \'%s\' centimeters" % x
